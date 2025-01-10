7f5f64f030a9e4d885485e18da92c437954ed0e0
MyMac <mymac@MyMacs-MBP.attlocal.net>
2025-01-09 20:19:15 -0600

    solved config error

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "public_subnet_cidr" {
     default     = "10.0.1.0/24"
 }
 
-variable "public_subnet_cidr" {
+variable "private_subnet_cidr" {
     description = "cidr block for public subnet"
     type        = string
     default     = "10.0.2.0/24"
