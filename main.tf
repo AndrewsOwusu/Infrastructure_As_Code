8dbc9b712e362a379c5857c1ba0d2b33ddc5cc95
MyMac <mymac@MyMacs-MBP.attlocal.net>
2025-01-14 03:00:42 -0600

    cidrhost error

--- main.tf
+++ main.tf
@@ -12,23 +12,10 @@ resource "aws_instance" "web_instance" {
   }
 }
 
-# Data block to fetch CIDR block for each subnet defined in locals
-data "aws_subnet" "selected_subnet_0" {
-  id = local.subnet_list[0]
-}
-
-# # Secondary Network Interface without using data block
-resource "aws_network_interface" "secondary_interface" {
-  subnet_id       = local.subnet_list[0]  #var.subnet_id
-  security_groups = [aws_security_group.web_sg.id]  # Replace with your actual security group ID
-#   private_ips_cidr_block = data.aws_subnet.selected_subnet_0.cidr_block
-
-#   # Automatically assign secondary private IPs using the subnet_cidr_block variable directly
-  private_ips = [for i in range(var.secondary_private_ip_count) : 
-    cidrhost(data.aws_subnet.selected_subnet_0.cidr_block, i + 10)
-  ]
-
-}
+# # Data block to fetch CIDR block for each subnet defined in locals
+# data "aws_subnet" "selected_subnet_0" {
+#   id = local.subnet_list[0]
+# }
 
 # webserver2_and_3
 resource "aws_instance" "secondary_web" {
@@ -40,18 +27,43 @@ resource "aws_instance" "secondary_web" {
   security_groups = [aws_security_group.web_sg.id]
   #associate_public_ip_address = true
 
-### attach network interface
-  network_interface {
-    network_interface_id = aws_network_interface.secondary_interface.id
-    device_index         = 1
-  }
-
   tags = {
     Name = "PublicWebServer_${count.index}"
   }
 }
 
+# Data block to fetch CIDR block for each subnet defined in locals
+data "aws_subnet" "selected_subnet_0" {
+  id = local.subnet_list[0]
+}
 
+# # Secondary Network Interface without using data block
+resource "aws_network_interface" "secondary_interface" {
+  count           = length(local.subnet_list)
+  subnet_id       = local.subnet_list[count.index]  #var.subnet_id
+  security_groups = [aws_security_group.web_sg.id]  # Replace with your actual security group ID
+#   private_ips_cidr_block = data.aws_subnet.selected_subnet_0.cidr_block
+
+#   # Automatically assign secondary private IPs using the subnet_cidr_block variable directly
+  private_ips = [for i in range(var.secondary_private_ip_count) : 
+    cidrhost(data.aws_subnet.selected_subnet_0.cidr_block, i + 10)
+  ]
+
+### attach network interface
+  attachment {
+    # instance = "PublicWebServer_${count.index}"
+    instance = "${aws_instance.secondary_web.id}"
+    device_index         = 1
+  }
+
+}
+
+
+# ### attach network interface
+#   network_interface {
+#     network_interface_id = aws_network_interface.secondary_interface.id
+#     device_index         = 1
+#   }
 
 # ---
 # # Secondary Network Interface without using data block
