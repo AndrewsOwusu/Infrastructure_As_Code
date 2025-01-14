bc15e30d2eab66b268b6f9fd20d2a06c6f2afe29
MyMac <mymac@MyMacs-MBP.attlocal.net>
2025-01-14 04:03:53 -0600

    fix for creating subnets before referencing

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web_instance" {
   ami             = "ami-0c695cc059ad095e9"  # Amazon Linux 2 AMI (Update if necessary)
   instance_type   = "t3.micro"
   subnet_id       = aws_subnet.public_subnet.id
-  secondary_private_ips  = local.secondary_private_ips 
+  secondary_private_ips  = var.secondary_private_ips 
   #key_name        = aws_key_pair.my_key_pair.key_name
   security_groups = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
