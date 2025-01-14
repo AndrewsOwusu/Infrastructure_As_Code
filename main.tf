07dd9cb43354289670cd3d172d71e9a57d29f5b5
MyMac <mymac@MyMacs-MBP.attlocal.net>
2025-01-14 02:02:25 -0600

    cidrhost error

--- main.tf
+++ main.tf
@@ -12,18 +12,20 @@ resource "aws_instance" "web_instance" {
   }
 }
 
-# data "aws_subnet" "subnet" {
-#     id = local.subnet_list[count.index]
-# }
+# Data block to fetch CIDR block for each subnet defined in locals
+data "aws_subnet" "selected_subnet_0" {
+  id = local.subnet_list[0]
+}
 
 # # Secondary Network Interface without using data block
 resource "aws_network_interface" "secondary_interface" {
   subnet_id       = local.subnet_list[0]  #var.subnet_id
   security_groups = [aws_security_group.web_sg.id]  # Replace with your actual security group ID
-
+  private_ips_cidr_block = data.aws_subnet.selected_subnet_0.cidr_block
+  
 #   # Automatically assign secondary private IPs using the subnet_cidr_block variable directly
   private_ips = [for i in range(var.secondary_private_ip_count) : 
-    cidrhost(local.subnet_list[0].id.cidr_block, i + 10)
+    cidrhost(private_ips_cidr_block, i + 10)
   ]
 
 }
