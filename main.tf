7d277d53c940d5bce73e13f9b4470eed1919455d
MyMac <mymac@MyMacs-MBP.attlocal.net>
2025-01-13 21:24:22 -0600

    ec2 instance type update

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 # EC2 Instance in the Public Subnet
 resource "aws_instance" "web_instance" {
   ami             = "ami-09373db3d247551a0"  # Amazon Linux 2 AMI (Update if necessary)
-  instance_type   = "t2.micro"
+  instance_type   = "t3.micro"
   subnet_id       = aws_subnet.public_subnet.id
   #key_name        = aws_key_pair.my_key_pair.key_name
   security_groups = [aws_security_group.web_sg.name]
