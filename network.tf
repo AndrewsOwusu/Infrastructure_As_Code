a07ac2136cf239b664674704c4f5ededfd046493
MyMac <mymac@MyMacs-MBP.attlocal.net>
2025-01-14 04:02:57 -0600

    fix for creating subnets before referencing

--- network.tf
+++ network.tf
@@ -20,7 +20,7 @@ resource "aws_subnet" "public_subnet" {
 }
 
 #Private_Subnet_1
-resource "aws_subnet" "private_subnet" {
+resource "aws_subnet" "private_subnet1" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1a"
@@ -30,7 +30,7 @@ resource "aws_subnet" "private_subnet" {
 }
 
 # Private_Subnet_2
-resource "aws_subnet" "private_subnet" {
+resource "aws_subnet" "private_subnet2" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "us-east-1a"
